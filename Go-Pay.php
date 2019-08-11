5b947872b3c361ff30ac7894ffd4a726e4f556e4
abhiyasaRamaS <53219632+abhiyasaRamaS@users.noreply.github.com>
2019-08-11 19:05:46 +0700

    Add files via upload
    Silahkan dipakai

---
+++ Go-Pay.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Welcome To Tools Prank :V
+Jangan Dibawa Serius serius amat nanti sakit
+Bwahaha
+Ngapain lu masuk kesini!!!
+*/
+
+echo "\n\e[0;31m]
+         .---.        .-----------
+        /     \  __  /    ------
+       / /     \(••)/    -----
+      //////   * \/ *   ---
+     //// / // :    : ---
+    // /   /  /*    *--
+   //          //..\\
++=============UU====UU=======================+
++              *//||\\*                       +
++               ****                         +
++ copryght by : MR.C4PUN6                    +
++ Wa : 089*******08                          +
++ Team : BCA-X66X,CTI,MARIANAS WEB TEAM      +
++ GitHub : https://github.com/abhiyasaRamaS  +
++============================================+
+                              Version 1.1.1\e[0m \r\n";
+echo "\n\e[0;36m]
+--==::{Nuyul Go-Pay}::==--";
+echo "\n\e[0;34m]Note 62xxx jangan 08xxx tod";
+echo "\n\e[0;33m]
+Nomer: \e[0m \r\n";
+$no = trim(fgets(STDIN, 1024));
+echo "\n\e[0;33m]
+Jumblah Go-Pay: \e[0m \r\n";
+$ju = trim(fgets(STDIN, 1024));
+echo "\n\e[0;32m]
+$no , Sukses Ngirim Saldo Go-Pay , $ju\e[0m \r\n";
+echo "\n\e[0;34m]
+Wait 2jam Gan\e[0m \r\n";
+echo "\n\e[0;34m]
+Kalo Gk Kekirim Anda Kurang Beruntung\e[0m \r\n";
+echo "\n\e[0;34m]
+Follow GitHub Gw\e[0m \r\n";
+?>
